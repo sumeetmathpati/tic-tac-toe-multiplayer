37c22c73d6f837aa1924b8dc3e89e3aa23064b1f
Sumeet Mathpati <sumeet221b@gmail.com>
2020-04-01 00:05:08 +0530

    initial commit

--- client.py
+++ client.py
@@ -1,30 +1,112 @@
-#!/usr/bin/python3           # This is client.py file
+#!/usr/bin/python3
 
 import socket
+import signal
+import sys
+from os import system, name 
+from termios import tcflush, TCIFLUSH
 
-# create a socket object
+# Box design string
+box = "   +   +   \n   |   |   \n   |   |   \n+--+---+--+\n   |   |   \n   |   |   \n   |   |   \n+--+---+--+\n   |   |   \n   |   |   \n   +   +   \n"
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-# get local machine name
-host = socket.gethostname()                           
+head_message = """
+    Welcome to tic-tac-toe
+    For Server: O
+    For client: X
+    Press ctrl+c to exit
+"""
 
-port = 9999
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
 
-# connection to hostname on the port.
-s.connect((host, port))       
+def intHandler(sig, frame):
+    print('Closing...')
+    s.close()
+    sys.exit(0)
+signal.signal(signal.SIGINT, intHandler)
 
-# Gaming stuff
-box = "   +   +   \n   |   |   \n   |   |   \n+--+---+--+\n   |   |   \n   |   |   \n+--+---+--+\n   |   |   \n   |   |   \n   +   +   \n"
+def changeBox(sign, position):
+   # Pass the position by counting from zero
+   if position in {13, 17, 21, 61, 65, 69, 109, 113, 117}:
+      return  box[0:position] + str(sign) + box[(position+1):]
 
+def receiveSignal(signalNumber, frame):
+    print('\nClosing...',)
+    return
 
-while True:
+if __name__ == "__main__":
 
-    # Receive no more than 1024 bytes
+    # create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+    # get local machine name
+    host = socket.gethostname()                           
+    port = 9991
+
+    # connection to hostname on the port.
+    s.connect((host, port))       
+
+    # Gaming stuff stuff
+    postion_to_index = {
+      "1": 13,
+      "2": 17,
+      "3": 21,
+      "4": 61,
+      "5": 65,
+      "6": 69,
+      "7": 109,
+      "8": 113,
+      "9": 117
+    }
+    # Record filled boxes; 0 for server; 1 for client; We're not using index 0
+    box_record = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+
+    # Receive message about who will play first; 0 for server and 1 for client
     msg = s.recv(1024)  
-    print(msg.decode('ascii'))                                   
+    first_play = (msg.decode('ascii'))
 
-    msg = input(">")
-    s.send(msg.encode('ascii'))
+    # Set send variable; if 1 send message; if 0 recieve message
+    if first_play == "0":
+        send = 0
+    else:
+        send = 1
 
-s.close()
-print (msg.decode('ascii'))
+    while True:
+
+        if send != 1:
+            msg = s.recv(1024)
+            msg = str(msg.decode('ascii'))
+            box = changeBox("O", postion_to_index[msg])
+            box_record[int(msg)] = 1
+            clear()
+            print(head_message)
+            print(box)  
+            send = 1                                 
+
+        if send == 1:
+            # Clear input buffer
+            tcflush(sys.stdin, TCIFLUSH)
+            input_recieved = str(input(">"))
+            print(input_recieved)
+            if input_recieved in ["1", "2", "3", "4", "5", "6", "7", "8", "9"] and box_record[int(input_recieved)] == -1:
+                box = changeBox("X", postion_to_index[input_recieved])
+                box_record[int(input_recieved)] = 0
+                s.send(input_recieved.encode('ascii'))
+                clear()
+                print(head_message)
+                print(box)
+                print("\nNow server's turn...\n")
+                send = 0
+            else:
+                continue
+        
+
+    s.close()
+
--- server.py
+++ server.py
@@ -1,36 +1,111 @@
 #!/usr/bin/python3 
 
-import socket                                         
+import socket       
+import random 
+import signal
+import sys
+from os import system, name 
+from termios import tcflush, TCIFLUSH
 
+# Box design string
+box = "   +   +   \n   |   |   \n   |   |   \n+--+---+--+\n   |   |   \n   |   |   \n   |   |   \n+--+---+--+\n   |   |   \n   |   |   \n   +   +   \n"
 
+head_message = """
+    Welcome to tic-tac-toe
+    For Server: O
+    For client: X
+    Press ctrl+c to exit
+"""
+
+# Server socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-# get local machine name 
-host = socket.gethostname()                           
+def intHandler(sig, frame):
+    print('\nClosing...')
+    serversocket.close()
+    sys.exit(0)
+signal.signal(signal.SIGINT, intHandler)
 
-port = 9999                                           
+def changeBox(sign, position):
+   # Pass the position by counting from zero
+   if position in {13, 17, 21, 61, 65, 69, 109, 113, 117}:
+      return box[0:position] + str(sign) + box[(position+1):]
 
-# bind to the port
-serversocket.bind((host, port))                                  
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
 
-# queue up to 5 requests
-serversocket.listen(5)         
+if __name__ == "__main__":
 
-clientsocket,addr = serversocket.accept() 
+   # get local machine name 
+   host = socket.gethostname()                           
+   port = 9991
+                      
+   # bind to the port
+   serversocket.bind((host, port))                                  
+   # queue up to 5 requests
+   serversocket.listen(5)         
+   clientsocket,addr = serversocket.accept() 
 
-while True:
-   # establish a connection
-   # clientsocket,addr = serversocket.accept()      
+   # Gaming stuff stuff
+   postion_to_index = {
+      "1": 13,
+      "2": 17,
+      "3": 21,
+      "4": 61,
+      "5": 65,
+      "6": 69,
+      "7": 109,
+      "8": 113,
+      "9": 117
+   }
 
-   # print("Got a connection from %s" % str(addr))
+   # Record filled boxes
+   box_record = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
+
+   # Who will play first; 0 for server and 1 for client
+   first_play = random.choice(["1", "0"])
+   print("First play: ", first_play)
+   # send variable; if 1 send message; if 0 recieve message
+   if first_play == "0":
+      send = 1
+   else:
+      send = 0
+   
+
+   # Tell client who will play first
+   clientsocket.send(first_play.encode('ascii'))
+
+   while True:
+      
+      if send == 1:
+         # Clear input buffer
+         tcflush(sys.stdin, TCIFLUSH)
+         input_recieved = str(input(">"))
+         if input_recieved in ["1", "2", "3", "4", "5", "6", "7", "8", "9"] and box_record[int(input_recieved)] == -1:
+            box = changeBox("X", postion_to_index[input_recieved])
+            clientsocket.send(input_recieved.encode('ascii'))
+            clear()
+            print(head_message)
+            print(box)
+            print("\nNow client's turn...\n")
+            send = 0
+         else:
+            continue
+      
+      if send != 1:
+         msg = clientsocket.recv(1024)
+         msg = str(msg.decode('ascii'))
+         box = changeBox("O", postion_to_index[msg])
+         clear()
+         print(head_message)
+         print(box)   
+         send = 1
+
     
-   #msg = 'Thank you for connecting'+ "\r\n"
+   clientsocket.close()
 
-    msg = input(">")
-    clientsocket.send(msg.encode('ascii'))
-
-    msg = clientsocket.recv(1024)  
-    print(msg.decode('ascii'))   
-
-    
-   #clientsocket.close()
